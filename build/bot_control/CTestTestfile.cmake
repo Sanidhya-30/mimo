0f0bba86dfde55b0fdddd184881f8b1e77b09ea0
Sanidhya Shrivastava <83417311+Sanidhya-30@users.noreply.github.com>
2024-04-07 02:17:41 +0530

    Delete build directory

--- build/bot_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lucifer/catkin_ws/src/bot_control
-# Build directory: /home/lucifer/catkin_ws/build/bot_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/bot_world/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lucifer/catkin_ws/src/bot_world
-# Build directory: /home/lucifer/catkin_ws/build/bot_world
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
